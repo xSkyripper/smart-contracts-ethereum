7537d16beeab71f4d8797f77a6cd9c72ac932348
xSkyripper <alexandru.cristea604@gmail.com>
2018-10-31 01:59:46 +0200

    Fix experiment 2 Voting

--- experiments/2_voting/voting.py
+++ experiments/2_voting/voting.py
@@ -19,7 +19,7 @@ def main(ethereum_net_addr):
 
     # Deploy the contract and get the address
     print("Deploying contract to network ...")
-    tx_hash = voting_contract.constructor().transact()
+    tx_hash = voting_contract.constructor([bytes('Alex', 'utf-8'), bytes('John', 'utf-8')]).transact()
     tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     print("Deployed voting contract: {}".format(
         tx_receipt.contractAddress
@@ -29,28 +29,31 @@ def main(ethereum_net_addr):
     voting = w3.eth.contract(address=tx_receipt.contractAddress,
                               abi=contract_interface['abi'],)
 
+    bytes_john = bytes('John', 'utf-8')
+    bytes_alex = bytes('Alex', 'utf-8')
+
     print("Voting for 'John' ...")
-    tx_hash = contract.functions.voteForCandidate('John').transact()
+    tx_hash = voting.functions.voteForCandidate(bytes_john).transact()
     w3.eth.waitForTransactionReceipt(tx_hash)
 
     print("Voting for 'John' ...")
-    tx_hash = contract.functions.voteForCandidate('John').transact()
+    tx_hash = voting.functions.voteForCandidate(bytes_john).transact()
     w3.eth.waitForTransactionReceipt(tx_hash)
 
     print("Voting for 'Alex' ...")
-    tx_hash = contract.functions.voteForCandidate('Alex').transact()
+    tx_hash = voting.functions.voteForCandidate(bytes_alex).transact()
     w3.eth.waitForTransactionReceipt(tx_hash)
 
     print("Voting for 'John' ...")
-    tx_hash = contract.functions.voteForCandidate('John').transact()
+    tx_hash = voting.functions.voteForCandidate(bytes_john).transact()
     w3.eth.waitForTransactionReceipt(tx_hash)
 
     print("Total votes for 'John': {}".format(
-        contract.functions.totalVotesFor('John').call()
+        voting.functions.totalVotesFor(bytes_john).call()
     ))
 
     print("Total votes for 'Alex': {}".format(
-        contract.functions.totalVotesFor('Alex').call()
+        voting.functions.totalVotesFor(bytes_alex).call()
     ))
 
 
