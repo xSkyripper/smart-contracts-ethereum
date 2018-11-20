5357fd85cfac12205e05d2332e8e4fccbcd889f4
xSkyripper <alexandru.cristea604@gmail.com>
2018-11-21 00:45:54 +0200

    Server: Initial Users CRUD

--- server/app/models.py
+++ server/app/models.py
@@ -1,17 +1,28 @@
-from app.shared import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+contracts = db.Table('contracts',
+    db.Column('contract_id', db.Integer, db.ForeignKey('contract.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
 
 class User(db.Model):
+    __table_args__ = {'sqlite_autoincrement': True}
     id = db.Column(db.Integer, primary_key=True)
     gov_id = db.Column(db.String(15), unique=True, nullable=False)
     first_name = db.Column(db.String(50), unique=False, nullable=False)
     last_name = db.Column(db.String(50), unique=False, nullable=False)
     ethereum_id = db.Column(db.String(512), unique=True, nullable=False)
+    contracts = db.relationship('Contract', secondary=contracts, lazy='subquery',
+                                backref=db.backref('users', lazy=True))
 
     def __repr__(self):
         return "<User %r>" % str(self.__dict__)
 
 
 class Contract(db.Model):
+    __table_args__ = {'sqlite_autoincrement': True}
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(15), unique=False, nullable=True)
     description = db.Column(db.String(512), unique=False, nullable=True)
--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 import os
-
-from flask import Flask
-from app.shared import db
+import sys
+from flask import Flask, request, render_template, redirect
+from app.models import *
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_FILE = "sqlite:///{}".format(os.path.join(PROJECT_DIR, "database.db"))
@@ -9,18 +9,37 @@ DB_FILE = "sqlite:///{}".format(os.path.join(PROJECT_DIR, "database.db"))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_FILE
 db.init_app(app)
+app.app_context().push()
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+@app.route('/users', methods=['POST'])
+def post_users():
+    new_user = User(gov_id=request.form['gov_id'],
+                    first_name=request.form['first_name'],
+                    last_name=request.form['last_name'],
+                    ethereum_id=request.form['gov_id'][::-1])
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/users')
 
 # Register route
 # Login route
-# get Contracts
+# Get Contracts
 
 # Users C R U D
 # Contracts C R U D
 # Contracts-Users
 
 if __name__ == '__main__':
-    app.run()
+    answer = input("Create database?(Y/N)")
+    if answer == "Y":
+        db.create_all()
+    app.run(debug=True)
