dc9c80045ddf6121cca82ccf581dc6544e435f84
Lucian Toma <tomalucian11@gmail.com>
2018-11-21 09:53:34 +0200

    Updated Logging

--- server/app/repo.py
+++ server/app/repo.py
@@ -1,12 +1,17 @@
 from app.models import *
+from app.decorators.LoggingAspect import before, after
 
 class UserRepository(object):
     def __init__(self):
         pass
 
+    @before
+    @after
     def get_users(self):
         return User.query.all()
-
+    
+    @before
+    @after
     def add_user(self, user):
         db.session.add(user)
         db.session.commit()
