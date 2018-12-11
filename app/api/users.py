9f3809d8fed43f70e4fdea4db0539535c0350020
xSkyripper <alexandru.cristea604@gmail.com>
2018-12-11 21:03:19 +0200

    Implement user GET

--- app/api/users.py
+++ app/api/users.py
@@ -3,10 +3,10 @@ from flask import request, current_app
 from flask_restplus import Resource
 from flask_login import current_user, login_required
 
+from app.models import User as UserModel
 from app.decorators import admin_required
 from app.api.security import require_auth
 from app.api import api_rest
-
 ns = api_rest.namespace('users', description='Users RESTful API')
 
 @ns.route('/<int:user_id>')
@@ -14,15 +14,26 @@ class User(Resource):
     # method_decorators = [login_required]
     # TODO: decorate each route for each resource based on requirements (login_required, admin_required)
 
-    @login_required
+    # @login_required
     def get(self, user_id):
         timestamp = datetime.utcnow().isoformat()
         current_app.logger.info(f'Received GET on user {user_id}')
-        return {
-            'message': f'Called GET user {user_id}',
-            'timestamp': timestamp,
-            }
 
+        user = UserModel.query.get(user_id)
+
+        if not user:
+            return dict(error=f"There is no user with Id {user_id}"), 404
+
+        return dict(
+            user=dict(
+                id=user.id,
+                gov_id=user.gov_id,
+                first_name=user.first_name,
+                last_name=user.last_name,
+                email=user.email,
+                ethereum_id=user.ethereum_id)), 200
+
+    # @login_required
     def put(self, user_id):
         timestamp = datetime.utcnow().isoformat()
         current_app.logger.info(f'Received PUT on user {user_id}')
@@ -31,6 +42,7 @@ class User(Resource):
             'timestamp': timestamp,
             }
 
+    # @admin_required
     def delete(self, user_id):
         timestamp = datetime.utcnow().isoformat()
         current_app.logger.info(f'Received DELETE on user {user_id}')
