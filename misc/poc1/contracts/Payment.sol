eefce0ac2ac48398a4fd13748a40640e999931a5
Lupu Silviu-Cristian <lupusilviu95@gmail.com>
2019-01-09 01:34:42 +0200

    Add better contract to poc1

--- misc/poc1/contracts/Payment.sol
+++ misc/poc1/contracts/Payment.sol
@@ -1,19 +1,54 @@
 pragma solidity ^0.4.17;
 
 contract Payment {
-	address[4] public payers;
+	
+	struct Payer {
+	    address addr;
+        bool payed;  // if true, that person already voted
+    }
+    
+    Payer[] payers;
+    uint amountDue;
+
+    constructor() public {
+        amountDue = 5;
+    	payers.push(Payer({
+    	addr: 0x6D4E5BAc0fc3fF8dF7A713DC0bB47cEF310a92cB,
+    	payed: false
+    	}));
+    	payers.push(Payer({
+    	addr: 0xeec7A4E837eEB5242E9d20976286a0cCD464EFb5,
+    	payed: false
+    	}));
+    	payers.push(Payer({
+    	addr: 0xe603AFeC589146C9A03786255b9386462EaeD9e5,
+    	payed: true
+    	}));
+
+    }
 
 	// Paying a bill
-	function pay(uint billId) public returns (uint) {
-	  require(billId >= 0 && billId <= 3);
-
-	  payers[billId] = msg.sender;
+	function pay(uint billId) public payable returns (uint) {
+	  for (uint p = 0; p < payers.length; p++) {
+            if (payers[p].addr == msg.sender) {
+      			require(!payers[p].payed, "Already payed!");
+      			payers[p].payed = true;
+            }
+        }
 
 	  return billId;
 	}
 	
+	function getAmountDue() returns (uint){
+		return amountDue;
+	}
+
 	// Retrieving the payers
-	function getPayers() public view returns (address[4]) {
-	  return payers;
+	function getPayers() public view returns (address[]) {
+	  address[] payers_addresses;
+	  return payers_addresses;
+	}
+	function getContractBalance() public returns (uint){
+	  return this.balance;
 	}
 }
