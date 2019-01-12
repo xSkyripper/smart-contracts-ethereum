d61ff0bd5017f61fa16f9c26d334e9e2cf35f961
xSkyripper <alexandru.cristea604@gmail.com>
2019-01-13 01:07:18 +0200

    Upgrade smart_contract to final -hopefully- version

--- app/managers/smart_contract.py
+++ app/managers/smart_contract.py
@@ -1,97 +1,96 @@
-from web3 import Web3
-from easysolc import Solc
-from app.models import Contract as ContractModel, User as UserModel
 import logging
 import json
 import os.path
+from web3 import Web3
+from easysolc import Solc
 
 logger = logging.getLogger(__name__)
 
-class SmartContractManager(object):
-    def __init__(self,
-                 eth_network_addr,
-                 owner,
-                 solc_path='',
-                 ):
-        self._solc = Solc(solc_path)
-        self._web3 = Web3(Web3.HTTPProvider(eth_network_addr))
-        self._web3.eth.defaultAccount = owner
 
-    @staticmethod
-    def _build_abi_path(contract_path):
+class Manager(object):
+    def __init__(self, web3_client):
+        self._web3 = web3_client
+
+    @classmethod
+    def build_abi_path(cls, contract_path):
         contract_dirname = os.path.dirname(contract_path)
         contract_filename = os.path.basename(contract_path)
         contract_abi_filename = os.path.splitext(contract_filename)[0] + '_abi.json'
 
         return os.path.join(contract_dirname, contract_abi_filename)
 
-    @staticmethod
-    def _save_contract_abi(contract_abi, contract_abi_path):
+    @classmethod
+    def persist_contract_abi(cls, contract_abi, contract_abi_path):
         with open(contract_abi_path, 'w+') as fd:
             json.dump(contract_abi, fd, indent=4)
+    
+    @classmethod
+    def save_contract_abi(cls, contract_abi, contract_path):
+        contract_abi_path = cls.build_abi_path(contract_path)
+        cls.persist_contract_abi(contract_abi, contract_abi_path)
+        logger.info('Written contract ABI to path "%s"', contract_abi_path)
 
-    @staticmethod
-    def get_contract_abi(contract_abi_path):
-        contract_abi = None
+    @classmethod
+    def get_contract_abi(cls, contract_abi_path):
         with open(contract_abi_path, 'r') as fd:
-            contract_abi = json.load(fd)
-        return contract_abi
+            return json.load(fd)
 
-    def _compile_contract(self, contract_file_path, contract_name):
-        logger.info('Creating contract from path "%s", named "%s"', contract_file_path, contract_name)
-        compiled_contract = self._solc.get_contract_instance(source=contract_file_path,
-                                                             contract_name=contract_name)
-        
+    @classmethod
+    def compile_contract(cls, contract_file_path, contract_name, solc_path=''):
+        logger.info('Creating contract from path "%s", named "%s"',
+                    contract_file_path, contract_name)
+        solc = Solc(solc_path)
+        compiled_contract = solc.get_contract_instance(source=contract_file_path,
+                                                       contract_name=contract_name)
+
         return compiled_contract
 
-    def _push_contract_to_ethereum(self, compiled_contract, amount_due):
+    def push_contract_to_ethereum(self, compiled_contract, *contract_args):
         contract_object = self._web3.eth.contract(abi=compiled_contract.abi,
                                                   bytecode=compiled_contract.bytecode)
 
-        tx_hash = contract_object.constructor(amount_due).transact()
+
+        tx_hash = contract_object.constructor(*contract_args).transact()
         tx_receipt = self._web3.eth.waitForTransactionReceipt(tx_hash)
 
-        logger.info('Contract with amount due "%d" with address "%s" pushed to Ethereum',
-                    amount_due, tx_receipt.contractAddress)
+        logger.info('Pushed contract with args "{}"'.format(*contract_args))
+        logger.info('Ethereum address of the contract is "%s"', tx_receipt.contractAddress)
         return tx_receipt.contractAddress, compiled_contract.abi
 
 
-    def create_contract(self, contract_path, contract_name, amount_due, persist=True):
-        compiled_contract = self._compile_contract(contract_path, contract_name)
+    def create_contract(self, contract_path, contract_name, *contract_args, save=True):
+        compiled_contract = self.compile_contract(contract_path, contract_name)
         logger.info('Created contract. Pushing to Ethereum network ...')
-        contract_addr, contract_abi = self._push_contract_to_ethereum(compiled_contract, amount_due)
+        contract_addr, contract_abi = self.push_contract_to_ethereum(compiled_contract, *contract_args)
+        if save:
+            self.save_contract_abi(contract_abi, contract_path)
 
-        if persist:
-            contract_abi_path = self._build_abi_path(contract_path)
-            logger.info("Writing ABI to path %s", contract_abi_path)
-            self._save_contract_abi(contract_abi, contract_abi_path)
-
-        return contract_addr, contract_abi
+        return contract_addr
 
 
 class PaymentContract(object):
-    def __init__(self, eth_network_addr, contract_eth_addr, contract_path, owner=None):
-        self._web3 = Web3(Web3.HTTPProvider(eth_network_addr))
-        self._owner = owner
-        self._contract_eth_addr = contract_eth_addr
-        self._contract_path = contract_path
-        self._contract_abi = SmartContractManager.get_contract_abi(
-            SmartContractManager._build_abi_path(contract_path))
-        self._contract = self._web3.eth.contract(address=contract_eth_addr, abi=self._contract_abi)
+    def __init__(self,
+                 web3_client,
+                 contract_eth_addr,
+                 contract_abi,
+                 owner=None):
+        self._web3 = web3_client
+        self.owner = owner or web3_client.eth.defaultAccount
+        self._contract = web3_client.eth.contract(address=contract_eth_addr, abi=contract_abi)
 
     def close(self):
-        tx_hash = self._contract.functions.close().transact({'from': self._owner})
+        tx_hash = self._contract.functions.close().transact({'from': self.owner})
         return self._web3.eth.waitForTransactionReceipt(tx_hash)
 
     def add_payer(self, user_eth_addr):
-        tx_hash = self._contract.functions.addPayer(user_eth_addr).transact({'from': self._owner})
+        tx_hash = self._contract.functions.addPayer(user_eth_addr).transact({'from': self.owner})
         return self._web3.eth.waitForTransactionReceipt(tx_hash)
 
     def get_payers(self):
         return self._contract.functions.getPayers().call()
 
     def remove_payer(self, user_eth_addr):
-        tx_hash = self._contract.functions.removePayer(user_eth_addr).transact({'from': self._owner})
+        tx_hash = self._contract.functions.removePayer(user_eth_addr).transact({'from': self.owner})
         return self._web3.eth.waitForTransactionReceipt(tx_hash)
 
     def pay(self, user_eth_addr, value):
--- stuff/testing1.py
+++ stuff/testing1.py
@@ -1,25 +1,25 @@
-from app.managers.smart_contract import SmartContractManager, PaymentContract
+from app.managers.smart_contract import Manager as SCManager, PaymentContract
 import IPython
 import logging
+from web3 import Web3
 
-l = logging.getLogger('smart_contract')
-logging.basicConfig(level=logging.INFO)
+logging.getLogger('smart_contract').setLevel(level=logging.INFO)
 
-ethereum_net_addr = 'http://127.0.0.1:7545'
-ethereum_default_account = '0xA6115D445B2D3DD2EBF9e7daEC2A135b4F750d02'
+web3_client = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
+web3_client.eth.defaultAccount = '0xA6115D445B2D3DD2EBF9e7daEC2A135b4F750d02'
+
 contract_file_path = 'app/contracts_files/Payment.sol'
 contract_name = 'Payment'
 contract_amount_due = 1000000000000000000 # 1 ether
+scm = SCManager(web3_client)
+contract_eth_addr = scm.create_contract(contract_file_path, contract_name, contract_amount_due)
 
-smc = SmartContractManager(ethereum_net_addr, owner=ethereum_default_account)
-
-# contract_eth_addr, contract_abi = smc.create_contract(contract_file_path, contract_name, contract_amount_due)
-contract_eth_addr = '0x332C66F0444D13FbEeB5602cb24bffac3c636A78'
-
-pc = PaymentContract(eth_network_addr=ethereum_net_addr,
-                     contract_eth_addr=contract_eth_addr,
-                     contract_path=contract_file_path,
-                     owner=ethereum_default_account)
+# contract_eth_addr = '0x1D52CE6C16748aA1DbC7B0Fad696a3e1dd8549e6'
+# contract_abi_path = SCManager.build_abi_path(contract_file_path)
+# contract_abi = SCManager.get_contract_abi(contract_abi_path)
+# pc = PaymentContract(web3_client=web3_client,
+#                      contract_eth_addr=contract_eth_addr,
+#                      contract_abi=contract_abi)
 
 # r = pc.add_payer('0x2d337E1AB8AD8a5DBFfD6aC06f8E55F2E09bDf23')
 # r = pc.add_payer('0x32ae6e2418e99CdeFAE8173EA6B631F4891deCb4')
@@ -27,4 +27,4 @@ pc = PaymentContract(eth_network_addr=ethereum_net_addr,
 # amount_due = pc.get_amount_due()
 # contract_balance = pc.get_contract_balance()
 
-IPython.embed()
+# IPython.embed()
